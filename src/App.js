0d33560573dc0fe5c4e4117ae7ebdba0c7dcc701
DESKTOP-LD0LDCE\Blendar Kabashi <blendar.h.kabashi@gmail.com>
2021-02-28 19:32:09 +0100

    button component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from './components/Header.js'
 import WinnersSection from './components/WinnersSection.js'
 import Footer from './components/Footer.js'
+import Button from './components/Button.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <WinnersSection />
+      <Button text="JOIN NOW TO GET YOUR WELCOME PACK" />
       <Footer />
     </div>
   );
---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import './Button.scss';
+
+const Button = ({text}) => {
+    return (
+        <div className="join-btn">
+            <a href="">{text}</a>
+        </div>
+    )
+}
+
+export default Button
